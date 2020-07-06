3f69ed6277dd6eeaa0a4e7773c51f092e79f21df
Valenti Sanz <valenti@alfred.home>
2020-07-06 22:47:38 +0200

    first ig commit

--- pyInstagram.py
+++ pyInstagram.py
@@ -1,24 +1,44 @@
 from selenium import webdriver
+import time
+username = input("Enter username:")
+password = input("Enter password:")
+userToSearch = input("Enter the user to search:")
 
 driver = webdriver.Chrome("/Users/valenti/Documents/chromedriver")
 driver.get("https://www.instagram.com/")
-# driver.execute_script("window.open('https://www.google.com');")
 driver.maximize_window()
 
-username = input("Enter username:")
-password = input("Enter password:")
 
-# Guardar els inputs i el boto de login
-username_box = driver.find_element_by_xpath(
-    "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
-password_box = driver.find_element_by_xpath(
-    "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
-btn = driver.find_element_by_xpath(
-    "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[4]/button")
+def login():
+    # Save the elements with which we are going to interact
+    username_box = driver.find_element_by_xpath(
+        "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
+    password_box = driver.find_element_by_xpath(
+        "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
+    btn = driver.find_element_by_xpath(
+        "//*[@id='react-root']/section/main/article/div[2]/div[1]/div/form/div[4]/button")
 
-# Introduir credencials i fer click
-username_box.send_keys(username)
-password_box.send_keys(password)
-btn.click()
+    # Enter username and password given by the user and sign in
+    username_box.send_keys(username)
+    password_box.send_keys(password)
+    btn.click()
+    time.sleep(3)
+    searchUser()
 
+
+def searchUser():
+    # Search for the entered user
+    search_bar = driver.find_element_by_xpath(
+        "//*[@id='react-root']/section/nav/div[2]/div/div/div[2]/input")
+    search_bar.send_keys(userToSearch)
+    time.sleep(3)
+    # Click the first result of the users list
+    first_result = driver.find_element_by_xpath(
+        "//*[@id='react-root']/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]")
+    first_result.click()
+
+
+login()
+input()
+driver.quit()
 print("Success :)")
